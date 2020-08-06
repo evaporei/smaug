9d3fc3c0e2eafb245a2ee8a0c54b08e0d1222742
Otavio Pace <otaviopp8@gmail.com>
2020-08-06 20:10:11 -0300

    main: basic actix-web with index route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
