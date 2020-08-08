2580d34a02da80d563b679d3950cf86898dc99c3
Otavio Pace <otaviopp8@gmail.com>
2020-08-08 10:40:39 -0300

    crux: remove clojure code in favor of Docker

--- crux-db-node/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to crux-db-node
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- crux-db-node/test/crux_db_node/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns crux-db-node.core-test
-  (:require [clojure.test :refer :all]
-            [crux-db-node.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
