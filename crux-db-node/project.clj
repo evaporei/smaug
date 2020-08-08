9af06ce1233ed09ee123d2c3eb7abcd5e2ec3979
Otavio Pace <otaviopp8@gmail.com>
2020-08-08 10:39:44 -0300

    kafka ultra wip, probably not possible

--- crux-db-node/project.clj
+++ crux-db-node/project.clj
@@ -5,7 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/crux-core "20.07-1.10.0-beta"]
-                 [juxt/crux-http-server "20.07-1.10.0-alpha"]]
+                 [juxt/crux-http-server "20.07-1.10.0-alpha"]
+                 [juxt/crux-rocksdb "20.08-1.10.1-beta"]
+                 [juxt/crux-kafka "20.08-1.10.1-beta"]]
   :main ^:skip-aot crux-db-node.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- crux-db-node/src/crux_db_node/core.clj
+++ crux-db-node/src/crux_db_node/core.clj
@@ -3,13 +3,15 @@
   (:require [crux.api :as crux]
             [clojure.java.io :as io]))
 
-(defn start-standalone-http-node [port storage-dir]
-  (crux/start-node {:crux.node/topology '[crux.standalone/topology crux.http-server/module]
+(defn start-node [http-port kafka-port storage-dir]
+  (crux/start-node {:crux.node/topology '[crux.standalone/topology crux.kv.rocksdb/kv-store crux.http-server/module]
                     :crux.standalone/event-log-dir (io/file storage-dir "event-log")
+                    :crux.standalone/event-log-kv-store 'crux.kv.rocksdb/kv
+                    :crux.kafka/bootstrap-servers (str "localhost:" kafka-port)
                     :crux.kv/db-dir (io/file storage-dir "indexes")
-                    :crux.http-server/port port}))
+                    :crux.http-server/port http-port}))
 
 (defn -main
   [& args]
-  (println "Starting CruxDB at 3000 port")
-  (start-standalone-http-node 3000 "store"))
+  (println "Starting CruxDB, with HTTP server at 3000 port")
+  (start-node 3000 9092 "store"))
